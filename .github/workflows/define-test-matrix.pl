f6766b488552d1de3f6a0e6ec9cd0c0236afcd6e
Dave Rolsky <autarch@urth.org>
2025-04-04 13:44:31 +0200

    Add support for running a threaded Perl for tests as well

---
+++ .github/workflows/define-test-matrix.pl
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+use autodie;
+
+use JSON::PP qw( encode_json );
+
+my $min_perl     = shift;
+my $with_threads = shift;
+
+my @jobs = (
+    {
+        name           => 'Linux',
+        'runs-on'      => 'ubuntu-latest',
+        'perl-version' => $min_perl,
+    },
+    {
+        name           => 'macOS',
+        'runs-on'      => 'macos-latest',
+        'perl-version' => 'latest',
+    },
+    {
+        name           => 'Windows',
+        'runs-on'      => 'windows-latest',
+        'perl-version' => 'latest',
+    }
+);
+
+if ( $with_threads eq 'true' ) {
+    push @jobs, {
+        name           => 'Linux (threaded Perl)',
+        'runs-on'      => 'ubuntu-latest',
+        'perl-version' => 'latest',
+        'multi-thread' => $JSON::PP::true,
+    };
+}
+
+my %matrix = (
+    'os-and-perl' => \@jobs,
+);
+
+open my $fh, '>>', $ENV{GITHUB_OUTPUT};
+print {$fh} "matrix=" . encode_json( \%matrix ) . "\n"
+    or die "Cannot write to $ENV{GITHUB_OUTPUT}: $!";
+close $fh;
