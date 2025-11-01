4eb9b024d5b6b5ed41650412f49ba13e6405fd30
Dave Rolsky <autarch@urth.org>
2025-11-01 11:42:24 -0500

    Tidy all code with the latest `Perl::Tidy`

--- .github/actions/set-up-perl/define-cache-paths.pl
+++ .github/actions/set-up-perl/define-cache-paths.pl
@@ -3,14 +3,14 @@ use warnings;
 use autodie;
 
 my $perl_version = shift @ARGV;
-my $cache_extra = shift @ARGV;
+my $cache_extra  = shift @ARGV;
 
 my @cache = @INC;
 if ($cache_extra) {
     push @cache, "/opt/hostedtoolcache/perl/$perl_version/x64/$cache_extra";
 }
 
-my $cache = join q{}, map { "$_\n" } @cache;
+my $cache = join q{}, map {"$_\n"} @cache;
 
 open my $fh, '>>', $ENV{GITHUB_OUTPUT};
 print {$fh} "cache-paths<<EOF-1\n$cache\nEOF-1\n"
